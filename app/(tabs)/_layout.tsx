a426a53dfaca345a42ba9a121340ae3bf9501da3
Daniel Allen <scouseexplorer57@gmail.com>
2026-02-07 15:03:16 +0000

    i removed unnecesaary elements offf the footer

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,70 +16,90 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            height: 60 + insets.bottom,
-            paddingBottom: insets.bottom + 3,
-            paddingTop: 5,
-            borderTopWidth: 1,
-            borderTopColor: '#E5E5E5',
-          },
-          android: {
-            height: 70,
-            paddingBottom: 90,
-            paddingTop: 10,
-            borderTopWidth: 1,
-            borderTopColor: '#E5E5E5',
-            elevation: 8,
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            right: 0,
-          },
-        }),
+      tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      headerShown: false,
+      tabBarButton: HapticTab,
+      tabBarBackground: TabBarBackground,
+      tabBarStyle: Platform.select({
+        ios: {
+        height: 32 + insets.bottom,
+        paddingBottom: insets.bottom,
+        paddingTop: 2,
+        borderTopWidth: 1,
+        borderTopColor: '#E5E5E5',
+        },
+        android: {
+        height: 32 + insets.bottom,
+        paddingBottom: insets.bottom,
+        paddingTop: 2,
+        borderTopWidth: 1,
+        borderTopColor: '#E5E5E5',
+        elevation: 8,
+        },
+      }),
       }}
     >
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Login',
-          href: null,
-        }}
+      name="index"
+      options={{
+        title: 'Login',
+        href: null, // Hide from tabs
+      }}
       />
       
       <Tabs.Screen
-        name="homepage"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={24} name="house.fill" color={color} />,
-        }}
+      name="homepage"
+      options={{
+        title: 'Home',
+        tabBarIcon: ({ color }) => <IconSymbol size={24} name="house.fill" color={color} />,
+      }}
       />
       
       <Tabs.Screen
-        name="aichat"
-        options={{
-          title: 'Chat',
-          tabBarIcon: ({ color }) => <IconSymbol size={24} name="message.fill" color={color} />,
-        }}
+      name="flashcards"
+      options={{
+        title: 'Flashcards',
+        href: null, // Hide from tabs - accessed from homepage
+      }}
+      />
+      
+      <Tabs.Screen
+      name="aichat"
+      options={{
+        title: 'Chat',
+        tabBarIcon: ({ color }) => <IconSymbol size={24} name="message.fill" color={color} />,
+      }}
       />
       
       <Tabs.Screen 
-        name="bookmarks" 
-        options={{
-          title: 'Bookmarks',
-          tabBarIcon: ({ color }) => <IconSymbol size={24} name="bookmark.fill" color={color} />,
-        }}
+      name="bookmarks" 
+      options={{
+        title: 'Bookmarks',
+        tabBarIcon: ({ color }) => <IconSymbol size={24} name="bookmark.fill" color={color} />,
+      }}
       />
       <Tabs.Screen
-        name="userprofile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={24} name={Platform.OS === 'ios' ? 'person.crop.circle.fill' : 'person.circle'} color={color} />,
-        }}
+      name="userprofile"
+      options={{
+        title: 'Profile',
+        tabBarIcon: ({ color }) => <IconSymbol size={24} name={Platform.OS === 'ios' ? 'person.crop.circle.fill' : 'person.circle'} color={color} />,
+      }}
+      />
+
+      <Tabs.Screen
+      name="quiz"
+      options={{
+        title: 'Quiz',
+        href: null, // Hide from tabs - accessed from homepage
+      }}
+      />
+
+      <Tabs.Screen
+      name="roadsigns"
+      options={{
+        title: 'Road Signs',
+        href: null, // Hide from tabs - accessed from homepage
+      }}
       />
     </Tabs>
   );
